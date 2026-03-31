af6c7178ab365a1c527fe7eb7d36aba7e53e504b
AdityaPatil1601 <adityapatil111000>
2026-04-01 00:15:16 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
 def add(a, b):
+    logging.info("Addition operation performed")
     return a + b
 
-if __name__ == "__main__":
-    print(add(2, 3))
